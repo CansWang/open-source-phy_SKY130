2988365d8e8439f667ef00fb01584f6b8e29d2a3
CansWang <canswang@stanford.edu>
2021-05-15 03:29:30 -0700

    verilog netlist added

---
+++ vlog_netlist/osc/osc_core.sv
@@ -0,0 +1,24 @@
+module osc_core (
+    input glob_en,
+    input [3:0] delay_con_lsb,
+    input [6:0] delay_con_msb,
+    input [3:0] con_perb,
+
+    output osc_000,
+    output osc_036,
+    output osc_072,
+    output osc_144,
+    output osc_180
+
+// 
+// waiting to integrate the ref_injector
+//  
+
+// 
+// buffered output phase, goes to phase blender
+// 
+
+)
+
+
+
---
+++ vlog_netlist/osc/osc_design/rtl/gen_tcl.py
@@ -0,0 +1,37 @@
+import os
+
+# convenience function that creates a nicely formatted TCL list
+def tcl_list(vals, indent='    ', nl='\n'):
+    retval = f' \\{nl}{indent}'.join(f'{{{val}}}' for val in vals)
+    retval = '[list ' + retval + f' \\{nl}]'
+    return retval
+
+# shorten variable name for readability
+e = os.environ
+
+# determine the search path for include files
+
+# build up a list of source files
+file_list = []
+file_list += [e['VLOG_HOME']+'/all/ff_c.sv']
+file_list += [e['VLOG_HOME']+'/all/qr_4t1_mux_top.sv']
+file_list += [e['VLOG_HOME']+'/all/qr_mux_fixed.sv']
+file_list += [e['VLOG_HOME']+'/all/tx_inv.sv']
+
+print('loaded verilog source file list:', file_list)
+
+# create the text of the TCL script
+output = f'''\
+set_app_var search_path $search_path
+
+# Read design
+set file_list {tcl_list(file_list)}
+analyze -format sverilog $file_list
+
+# Elaborate the design target
+elaborate qr_4t1_mux_top
+'''
+
+# write output text
+with open('outputs/read_design.tcl', 'w') as f:
+    f.write(output)
