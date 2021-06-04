23f55be728be88080770b89257759ebfbc5069e0
estelle <estel@stanford.edu>
2021-06-04 00:03:51 -0700

    add to gen_init

--- digital_top/cadence-innovus-init/gen_init.py
+++ digital_top/cadence-innovus-init/gen_init.py
@@ -3,7 +3,7 @@ with open('innovus-foundation-flow/init.tcl', 'w') as f:
 set init_abstract_name ""
 set init_verilog "./inputs/design.v"
 set init_mmmc_file "innovus-foundation-flow/view_definition.tcl"
-set init_lef_file "inputs/adk/rtk-tech.lef inputs/adk/stdcells.lef inputs/adk/stdcells.lef inputs/adk/rtk-tech.lef"
+set init_lef_file "inputs/adk/rtk-tech.lef inputs/adk/stdcells.lef inputs/adk/stdcells.lef inputs/adk/rtk-tech.lef inputs/prbs_generator_syn.lef inputs/hr_16t4_mux_top.lef inputs/qr_4t1_mux_top.lef"
 set init_top_cell "digital_top"
 set init_gnd_net "AVSS CVSS DVSS"
 set init_pwr_net "AVDD CVDD DVDD"''')
